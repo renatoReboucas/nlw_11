c14ad9af9d838a8dbf474d802d4f79c460e43e15
C1325638 Renato Reboucas Da Silva <c1325638@interno.bb.com.br>
2023-05-22 13:51:24 -0300

    iniciando autenticacao web github

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
-import { User } from 'lucide-react'
-import nlwLogo from '../assets/logo.svg'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { Signin } from '@/components/Signin'
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-cols-2">
@@ -8,63 +9,13 @@ export default function Home() {
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16 ">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
-
-        <a
-          href="#"
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="max-w-[140px] text-sm leading-snug">
-            {' '}
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-
-        <div className="space-y-5 ">
-          <Image src={nlwLogo} alt="NLW logo" />
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="  text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-          <a
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-            href="#"
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a
-            target="_blank"
-            className="underline hover:text-gray-100"
-            href="https://www.rocketseat.com.br"
-            rel="noreferrer"
-          >
-            Rocketseat
-          </a>{' '}
-        </div>
+        <Signin />
+        <Hero />
+        <Copyright />
       </div>
-
       {/* right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="#" className="underline hover:text-gray-50">
-              criar agora
-            </a>{' '}
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </div>
   )
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        className="underline hover:text-gray-100"
+        href="https://www.rocketseat.com.br"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>{' '}
+    </div>
+  )
+}
