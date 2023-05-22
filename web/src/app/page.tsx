63ff8b00fc8e3830bfef3f4a137f280304a7b554
C1325638 Renato Reboucas Da Silva <c1325638@interno.bb.com.br>
2023-05-22 16:00:34 -0300

    login web concluido

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,18 @@
+import { cookies } from 'next/headers'
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { Signin } from '@/components/Signin'
+import { Profile } from '@/components/Profile'
 export default function Home() {
+  const isAutenticated = cookies().has('token')
   return (
     <div className="grid min-h-screen grid-cols-2">
       {/* left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16 ">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
-        <Signin />
+        {isAutenticated ? <Profile /> : <Signin />}
         <Hero />
         <Copyright />
       </div>
